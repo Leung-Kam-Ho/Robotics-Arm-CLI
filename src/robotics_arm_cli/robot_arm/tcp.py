0db8b1f8ba610436140b6d98520f01641a08d55b
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2026-02-20 20:22:29 +0800

    Refactor TCP class to set socket timeout during initialization and restore SKILL.md for robotic arm control guidance

--- src/robotics_arm_cli/robot_arm/tcp.py
+++ src/robotics_arm_cli/robot_arm/tcp.py
@@ -1,8 +1,10 @@
 import socket
 
+
 class Tcp:
     def __init__(self, host):
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s.settimeout(5)
         self.s.connect((host, 10003))
         
     def send(self, msg):
