6792eefd6ef3ed27c670e2067eae2ada9c921a58
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2026-02-20 16:27:40 +0800

    Refactor main function to use connectToRobotArm and improve argument handling

--- src/robotics_arm_cli/app.py
+++ src/robotics_arm_cli/app.py
@@ -2,15 +2,37 @@ from robotics_arm_cli.robot_arm.robot_arm import RobotArm
 import argparse
 
 
+def connectToRobotArm(ip_address: str, base_offset: int, speed: float = 0.4) -> RobotArm:
+    ra = RobotArm(host=ip_address, base_offset=base_offset)
+    ra.set_speed(speed)  # set speed to value from args.speed
+    print("---- Robot Arm Connection Details ---")
+    print(f"Connected to robot arm at {ip_address}.")
+    print(f"Current Speed: {speed} m/s")
+    print(f"Current position: {ra.read_cartesian_pos()}")
+    return ra
+
+
+def float_range(min_value, max_value):
+    def checker(arg):
+        try:
+            f = float(arg)
+        except ValueError:
+            raise argparse.ArgumentTypeError("must be a floating point number")
+        if f < min_value or f > max_value:
+            raise argparse.ArgumentTypeError(
+                f"must be in range [{min_value} .. {max_value}]"
+            )
+        return f
+    return checker
+
+
 def main():
     parser = argparse.ArgumentParser(description="Run the robotics arm behavior tree example.")
     parser.add_argument("--ip", type=str, required=True, help="IP address of the robot arm")
-    parser.add_argument("--offset", type=float, required=True, help="Base offset for the robot arm in degrees")
+    parser.add_argument("--offset", type=int, required=True, help="Base offset for the robot arm in degrees")
+    parser.add_argument("--speed", type=float_range(0.1, 1.0), default=0.4, help="Speed for the robot arm (default: 0.4 m/s)")
     args = parser.parse_args()
-    ra = RobotArm(host=args.ip, base_offset=args.offset)
-    ra.set_speed(0.4)  # set speed to 40mm/s
-    print(f"Connected to robot arm at {args.ip}.")
-    print(f"Current position: {ra.read_cartesian_pos()}")
+    ra = connectToRobotArm(args.ip, args.offset, args.speed)
 
 
 if __name__ == "__main__":
