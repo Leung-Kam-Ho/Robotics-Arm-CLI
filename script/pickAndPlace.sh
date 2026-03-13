0fd562df6cebcde5a5309cbd843390168161804c
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2026-03-13 17:13:00 +0800

    Refactor pickAndPlace.sh to update movement coordinates and improve gripper operations

--- script/pickAndPlace.sh
+++ script/pickAndPlace.sh
@@ -1,25 +1,51 @@
 while true; do
+# d7d5
 rac 192.168.1.64 init
 rac 192.168.1.64 gripper --gripper-angle 60
-rac 192.168.1.64 move --coords 185 15 0 0 0 0
+rac 192.168.1.64 move --coords 185 15 -40 0 0 0
 rac 192.168.1.64 move --coords 185 15 65 0 0 0
 rac 192.168.1.64 gripper --gripper-angle 0
-rac 192.168.1.64 move --coords 185 15 0 0 0 0
+rac 192.168.1.64 move --coords 185 15 -40 0 0 0
 
-rac 192.168.1.64 move --coords 241 15 0 0 0 0
-rac 192.168.1.64 move --coords 241 15 65 0 0 0
+rac 192.168.1.64 move --coords 297 15 -40 0 0 0
+rac 192.168.1.64 move --coords 297 15 65 0 0 0
 rac 192.168.1.64 gripper --gripper-angle 60
-rac 192.168.1.64 move --coords 241 15 0 0 0 0
+rac 192.168.1.64 move --coords 297 15 -40 0 0 0
 rac 192.168.1.64 init
 
-rac 192.168.1.64 move --coords 241 15 0 0 0 0
-rac 192.168.1.64 move --coords 241 15 65 0 0 0
+# g8f6
+rac 192.168.1.64 move --coords 129 -158 -40 0 0 0
+rac 192.168.1.64 move --coords 129 -158 65 0 0 0
 rac 192.168.1.64 gripper --gripper-angle 0
-rac 192.168.1.64 move --coords 241 15 0 0 0 0
+rac 192.168.1.64 move --coords 129 -158 -40 0 0 0
 
-rac 192.168.1.64 move --coords 185 15 0 0 0 0
+rac 192.168.1.64 move --coords 241 -102 -40 0 0 0
+rac 192.168.1.64 move --coords 241 -102 65 0 0 0
+rac 192.168.1.64 gripper --gripper-angle 60
+rac 192.168.1.64 move --coords 241 -102 -40 0 0 0
+rac 192.168.1.64 init
+
+# d5d7
+rac 192.168.1.64 move --coords 297 15 -40 0 0 0
+rac 192.168.1.64 move --coords 297 15 65 0 0 0
+rac 192.168.1.64 gripper --gripper-angle 0
+rac 192.168.1.64 move --coords 297 15 -40 0 0 0
+
+rac 192.168.1.64 move --coords 185 15 -40 0 0 0
 rac 192.168.1.64 move --coords 185 15 65 0 0 0
 rac 192.168.1.64 gripper --gripper-angle 60
-rac 192.168.1.64 move --coords 185 15 0 0 0 0
+rac 192.168.1.64 move --coords 185 15 -40 0 0 0
+rac 192.168.1.64 init
+
+# f6g8
+rac 192.168.1.64 move --coords 241 -102 -40 0 0 0
+rac 192.168.1.64 move --coords 241 -102 65 0 0 0
+rac 192.168.1.64 gripper --gripper-angle 0
+rac 192.168.1.64 move --coords 241 -102 -40 0 0 0
+
+rac 192.168.1.64 move --coords 129 -158 -40 0 0 0
+rac 192.168.1.64 move --coords 129 -158 65 0 0 0
+rac 192.168.1.64 gripper --gripper-angle 60
+rac 192.168.1.64 move --coords 129 -158 -40 0 0 0
 rac 192.168.1.64 init
 done
